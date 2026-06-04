28328f5a8dbdf12cd4c8d1c7440aaf1c4a2c67fe
BShrestha <riderbkas@gmail.com>
2026-06-04 01:41:08 -0400

    changed migration customer to salesmen

---
+++ prisma/migrations/20260603000000_rename_customer_to_salesman/migration.sql
@@ -0,0 +1,5 @@
+-- Rename Customer table to Salesman
+ALTER TABLE "Customer" RENAME TO "Salesman";
+
+-- Rename CustomerPayment table to SalesmanPayment
+ALTER TABLE "CustomerPayment" RENAME TO "SalesmanPayment";
